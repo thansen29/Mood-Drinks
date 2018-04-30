813a66abcf872530686228fcaf9c0e3dc268a6b0
Thomas Hansen <thansen1694@gmail.com>
2018-04-30 13:06:15 -0400

    Protect client id and secret

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,44 +2,29 @@ require 'rest-client'
 
 class SearchController < ApplicationController
   def search
-    token = self.authorize()
     artist = params['artist']
 
-    # check if artist already exists
     searched_artist = Artist.find_by(name: artist) 
-    # if it does, just return their genres
+
     if searched_artist 
       render json: { genres: searched_artist.genres.pluck(:name), cache: false }
       return;
     else 
-      # # create a new artist row
-      # new_artist = Artist.create(name: artist)
+      token = self.authorize()      
       genres_query = RestClient.get("https://api.spotify.com/v1/search?q=#{artist}&type=artist",
                               {"Authorization" => "Bearer #{token}"})
+      # should do some error check in the case where the search fails for whatever reason
   
       @genres = JSON.parse(genres_query.body)['artists']['items'][0]['genres']
 
       render json: { genres: @genres, cache: true }
-
-      # @genres.each do |genre|
-      #   # see if genre already exists
-      #   searched_genre = Genre.find_by(name: genre)
-      #   if searched_genre
-      #     # create the new row with that genre and the new artist
-      #     ArtistGenre.create(genre_id: searched_genre.id, artist_id: new_artist.id)
-      #   else 
-      #     # create new genre, create row with that genre and new artist
-      #     new_genre = Genre.create(name: genre)
-      #     ArtistGenre.create(genre_id: new_genre.id, artist_id: new_artist.id)          
-      #   end 
-      # end 
     end 
 
   end
 
   def authorize
-    client_id = '7c4536cf355f48158c3b16701da1d677';
-    client_secret = '76d7f6ea2566494bb80a7c8484489a27';
+    client_id = ENV['client_id']
+    client_secret = ENV['client_secret']    
 
     grant = Base64.strict_encode64("#{client_id}:#{client_secret}")
 
